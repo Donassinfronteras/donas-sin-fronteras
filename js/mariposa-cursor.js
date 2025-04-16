ad071c1619e9322e23f9f71450b758c00ab81ce2
Donassinfronteras <donassinfronteras@gmail.com>
2025-04-16 14:33:44 -0400

    Update mariposa-cursor.js

--- js/mariposa-cursor.js
+++ js/mariposa-cursor.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cursor = document.querySelector('.cursor-mariposa');
+const cursor = document.querySelector('.cursor-mariposa');
 
-  document.addEventListener('mousemove', e => {
-    cursor.style.left = `${e.clientX}px`;
-    cursor.style.top = `${e.clientY}px`;
-  });
+document.addEventListener('mousemove', (e) => {
+  const offsetX = 10; // ajusta si quieres que esté más cerca
+  const offsetY = 10;
+  cursor.style.left = `${e.clientX + offsetX}px`;
+  cursor.style.top = `${e.clientY + offsetY}px`;
 });
